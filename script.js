dd4f67221df831151a92e0115e875aa2228f30c8
chirag jain <jchirag0610@gmail.com>
2026-04-24 20:42:20 +0530

    minor corrections

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-let items = document.querySelectorAll(".item");
-let item_details = document.querySelectorAll(".item-detail");
-let details_close = document.querySelectorAll(".close");
-items.forEach((elem) => {
-    elem.addEventListener("click", () => {
-        console.log(elem.id);
-        item_details[elem.id].style.display = "block";
+function openCards() {
+    let items = document.querySelectorAll(".item");
+    let item_details = document.querySelectorAll(".item-detail");
+    let details_close = document.querySelectorAll(".close");
+    items.forEach((elem) => {
+        elem.addEventListener("click", () => {
+            item_details[elem.id].style.display = "block";
+        });
     });
-});
 
-details_close.forEach((elem) => {
-    elem.addEventListener("click", () => {
-        console.log(elem.id);
-        item_details[elem.id].style.display = "none";
+    details_close.forEach((elem) => {
+        elem.addEventListener("click", () => {
+            item_details[elem.id].style.display = "none";
+        });
     });
-});
+}
+openCards();
