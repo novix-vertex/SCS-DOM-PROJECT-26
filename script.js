8b29447ca069643793820026aa023067441fe5ff
chirag jain <jchirag0610@gmail.com>
2026-04-26 12:46:48 +0530

    todo list done

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+todolist = [];
+
 function openCards() {
     let items = document.querySelectorAll(".item");
     let item_details = document.querySelectorAll(".item-detail");
@@ -14,14 +16,48 @@ function openCards() {
         });
     });
 }
-function addTask() {
-
-}
-function showTaskList() {
-    const sum;
-    <div class="task" id="0">
-        <h2 class="task-title">Title 1</h2>
-        <Button class="mark-complete-bt">Mark Complete</Button>
-    </div>
-}
 openCards();
+
+
+const form = document.querySelector("form");
+form.addEventListener("submit", (e, idx) => {
+    e.preventDefault();
+    const title = document.querySelector("#todo-title");
+    const desc = document.querySelector("#todo-desc");
+    addTask(title, desc);
+});
+function addTask(title, desc) {
+    todolist.push({
+        "title": title.value,
+        "description": desc.value,
+        "isCompleted": false
+    });
+    title.value = "";
+    desc.value = "";
+    showTaskList();
+    title.focus();
+}
+const todo_list_items = document.querySelector(".todo-list-items");
+function showTaskList() {
+    let sum = '';
+    todolist.forEach((todo, idx) => {
+        sum += `<div class="task" id=${idx}>
+        <h2 class="task-title">${todo.title}</h2>
+        ${todo.isCompleted ? `<button id=${idx} class="completed-bt">Completed</button>` : `<button id=${idx} class="mark-complete-bt">Mark Complete</button>`}
+    </div>`;
+        todo_list_items.innerHTML = sum;
+    });
+}
+
+todo_list_items.addEventListener("click", (e) => {
+    if (e.target.classList.contains("mark-complete-bt")) {
+        markCompleted(e.target.id);
+    }
+});
+
+
+function markCompleted(idx) {
+    todolist[idx].isCompleted = true;
+    showTaskList();
+}
+
